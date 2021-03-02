b84d7fef51f092e65353bfde3da157599f6b44c0
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-03-02 16:51:23 +0100

    Add cookies smessage

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+var cookies = new bootstrap.Modal(document.getElementById('cookies'), {
+    keyboard: false
+})
+
+acceptCookies.addEventListener("click", () => {
+    localStorage.setItem("cookieBannerDisplayed", "true");
+    cookies.toggle();
+});
+
+setTimeout(() => {
+    if (!localStorage.getItem("cookieBannerDisplayed")) {
+        cookies.toggle();
+    }
+}, 2000);
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields //
 (function () {
     'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to //
     var forms = document.querySelectorAll('.needs-validation')
-    // Loop over them and prevent submission
+    // Loop over them and prevent submission //
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
