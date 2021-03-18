eabefd509fea660d25916a4fb4b7aed6a2252e28
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-03-18 11:04:28 +0100

    Updated v.2021

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
-var cookies = new bootstrap.Modal(document.getElementById('cookies'), {
-    keyboard: false
-})
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-acceptCookies.addEventListener("click", () => {
-    localStorage.setItem("cookieBannerDisplayed", "true");
-    cookies.toggle();
-});
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-setTimeout(() => {
-    if (!localStorage.getItem("cookieBannerDisplayed")) {
-        cookies.toggle();
-    }
-}, 2000);
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields //
-(function () {
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to //
-    var forms = document.querySelectorAll('.needs-validation')
-    // Loop over them and prevent submission //
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["concert", "demo", "streaming", "album", "video"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["concert", "demo", "streaming", "album", "video"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        video: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 1,
+                nav: false
+            },
+            1000:{
+                items: 1,
+                nav: false
             }
-            form.classList.add('was-validated')
-        }, false)
-    })
-})()
+        }
+    });
+});
